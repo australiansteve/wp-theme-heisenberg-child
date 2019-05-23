2c388985ca9b00c14a13982982e0a6e9562679a9
Steve <steve@australiansteve.com>
2019-05-22 21:52:14 -0300

    Clean up and reindent About Page template

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -5,49 +5,41 @@
 
 get_header(); ?>
 
-	<div class="content-container">
+<div class="content-container">
 	
-		<div class="grid-x grid-padding-x">
+	<div class="grid-x grid-padding-x">
+
+		<div class="small-12 cell">
+
+			<h1><?php the_field('about_page_title', 'option');?></h1>
 			
-			<div class="small-12 cell">
-
-				<h1>About</h1>
-			
-			</div>
-
-<?php
-if ( have_posts() ) :
-
-	while ( have_posts() ) :
-
-		the_post();
-		$theSubMenu = 'about-menu';
-		get_template_part( 'template-parts/sub-menu' ); 
-?>
-
-			<div class="small-12 cell" id="the-content">
-				
-<?php the_content(); ?>
-				
-			</div>
-<?php
-	endwhile;
-
-	the_posts_navigation();
-
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
-
-else :
-
-	echo esc_html( 'Sorry, no posts' );
-
-endif;
-?>
 		</div>
 
-	</div>
+		<?php
+		$theSubMenu = 'about-menu';
+		get_template_part( 'template-parts/sub-menu' ); 
+
+		if ( have_posts() ) :
+
+			while ( have_posts() ) :
+
+				the_post();
+				?>
+				<div class="small-12 cell" id="the-content">
+
+					<?php the_content(); ?>
+
+				</div>
+				<?php
+			endwhile;
+
+		endif;
+
+	endif;
+	?>
+</div>
+
+</div>
 
 <?php
 get_footer();
