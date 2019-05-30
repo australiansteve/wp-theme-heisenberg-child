4b53cd53cef7ca04993da5529c50c510c2ea0e63
Steve <steve@australiansteve.com>
2019-05-30 20:53:01 -0300

    Footer layout and styling mostly complete

--- functions.php
+++ functions.php
@@ -6,3 +6,59 @@
 namespace Heisenberg;
 
 require_once __DIR__ . '/src/enqueue.php';
+
+add_action('after_setup_theme', function(){
+	add_image_size( 'footer_logo_size', 300, 180, true );
+
+	register_nav_menu( 'social-menu', __( 'Social Media Menu', 'heisenberg' ) );
+	register_nav_menu( 'footer-menu', __( 'Secondary Footer Menu', 'heisenberg' ) );
+
+});
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+}
+
+add_filter('wp_nav_menu_objects', function( $items, $args ) {
+	
+	// loop
+	foreach( $items as &$item ) {
+		
+		// vars
+		$icon = get_field('icon', $item);
+		
+		// replace title with icon
+		if( $icon ) {
+			$title = $item->title;
+			$item->title = '<i class="fab fa-'.$icon.'" title="'.$title.'"></i>';
+			
+		}
+		
+	}
+	
+	// return
+	return $items;
+	
+}, 10, 2);
+
+
--- header.php
+++ header.php
@@ -15,7 +15,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/30900d1525.js"></script>
+
 	<?php wp_head(); ?>
 </head>
 
