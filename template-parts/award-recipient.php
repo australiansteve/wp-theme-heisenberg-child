a276c51ddb84c9a319e8171992c9eacfee4b6cbd
Steve <steve@australiansteve.com>
2019-08-06 09:21:19 -0300

    Award recipient styling

--- template-parts/award-recipient.php
+++ template-parts/award-recipient.php
@@ -1,6 +1,6 @@
 <div class ="grid-x recipient">
 	<div class="cell">
-		<div class="grid-x">
+		<div class="grid-x top">
 			<div class="cell">
 				<h4 class="name">
 					<?php the_sub_field('name'); ?>
@@ -17,8 +17,8 @@
 			</div>
 		</div>
 
-		<div class="grid-x">
-			<div class="cell medium-8 left-column">
+		<div class="grid-x bottom">
+			<div class="cell medium-7 left-column">
 			<?php 
 			$image = get_sub_field('photo');
 				$size = 'award_recipient'; // (thumbnail, medium, large, full or custom size)
@@ -28,7 +28,7 @@
 				}
 				?>
 			</div>
-			<div class="cell medium-4 right-column">
+			<div class="cell medium-5 right-column">
 
 				<div class="bio">
 					<?php the_sub_field('bio'); ?>
