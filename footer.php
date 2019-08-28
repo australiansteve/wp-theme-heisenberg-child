70b06fce1247e7bd295a8853972552a8609497c4
Steve <steve@australiansteve.com>
2019-08-27 22:53:31 -0300

    Add medium screen logo higher up in left column of footer

--- footer.php
+++ footer.php
@@ -9,68 +9,83 @@
  * @package Heisenberg
  */
 ?>
-				</div> <!-- .content-container -->
-			</div><!-- #content -->
-		</main><!-- .grid-container -->
+					</div> <!-- .content-container -->
+				</div><!-- #content -->
+			</main><!-- .grid-container -->
 
-		<footer class="grid-container">
+			<footer class="grid-container">
 
-			<div class="content-container">
+				<div class="content-container">
 
-				<div class="grid-x align-stretch">
+					<div class="grid-x align-stretch">
 
-					<div class="medium-4 cell" id="left-column">
-						<?php wp_nav_menu( [
-							'theme_location' => 'footer-left-menu',
-							'container' => '',
-							'menu_class' => 'menu vertical',
+						<div class="medium-4 cell" id="left-column">
+							<?php wp_nav_menu( [
+								'theme_location' => 'footer-left-menu',
+								'container' => '',
+								'menu_class' => 'menu vertical',
 							]); 
-						?>
-					</div>
-					
-					<div class="medium-4 cell" id="center-column">
-						<?php wp_nav_menu( [
-							'theme_location' => 'footer-center-menu',
-							'container' => '',
-							'menu_class' => 'menu vertical',
-							]); 
-						?>
-					</div>
-					
-					<div class="medium-4 cell" id="right-column">
-						<?php the_field('footer_right_text', 'option'	); ?>
-					</div>
-
-				</div>
-				<div class="grid-x">
-					<div class="cell" id="logo">
-						<a href="/">
+							?>
+							<div class="show-for-medium">
+								<a href="/">
 <?php 
 
-$image = get_field('footer_logo', 'option');
-$size = 'site_logo'; // (thumbnail, medium, large, full or custom size)
+	$image = get_field('footer_logo', 'option');
+	$size = 'site_logo'; // (thumbnail, medium, large, full or custom size)
 
-if( $image ) {
-	echo wp_get_attachment_image( $image, $size );
-}
+	if( $image ) {
+		echo wp_get_attachment_image( $image, $size );
+	}
 
 ?>
-						</a>
-					</div>
-				</div>
-				<div class="grid-x">
-					<div class="cell text-center" id="copyright">
-						<a href="https://weavercrawford.com" target="blank">
-							<i class="far fa-copyright"></i> <?php echo date("Y");?> Weaver Crawford Creative
-						</a>
-					</div>
-				</div>
-				
-			</div>
+								</a>
+							</div>
+						</div>
 
-		</footer>
-	</div><!-- .off-canvas-wrapper -->
+						<div class="medium-4 cell" id="center-column">
+							<?php wp_nav_menu( [
+								'theme_location' => 'footer-center-menu',
+								'container' => '',
+								'menu_class' => 'menu vertical',
+							]); 
+							?>
+						</div>
 
-<?php wp_footer(); ?>
-</body>
+						<div class="medium-4 cell" id="right-column">
+							<?php the_field('footer_right_text', 'option'	); ?>
+						</div>
+
+					</div>
+
+					<div class="grid-x">
+						<div class="cell show-for-small-only" id="logo">
+							<a href="/">
+			<?php 
+
+			$image = get_field('footer_logo', 'option');
+			$size = 'site_logo'; // (thumbnail, medium, large, full or custom size)
+
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+
+			?>
+							</a>
+						</div>
+					</div>
+					<div class="grid-x">
+						<div class="cell text-center" id="copyright">
+							<a href="https://weavercrawford.com" target="blank">
+								<i class="far fa-copyright"></i> <?php echo date("Y");?> Weaver Crawford Creative
+							</a>
+						</div>
+					</div>
+
+				</div>
+
+			</footer>
+		</div><!-- .off-canvas-wrapper -->
+
+		<?php wp_footer(); ?>
+	</body>
 </html>
