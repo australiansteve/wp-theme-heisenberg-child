9b21fff852da5c0601d44d0b445025734dad8987
Steve <steve@australiansteve.com>
2019-10-02 09:16:15 -0300

    Add social media menu and ACF field group to display icon to base child theme

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ namespace Heisenberg;
 
 require_once __DIR__ . '/src/enqueue.php';
 
-/* Add ACF Theme Settings pages */
-if( function_exists('acf_add_options_page') ) {
-	
+/* ACF related options & filters */
+if( class_exists('acf') ) :
+
 	acf_add_options_page(array(
 		'page_title'	=> 'Theme General Settings',
 		'menu_title'	=> 'Theme Settings',
@@ -17,4 +17,26 @@ if( function_exists('acf_add_options_page') ) {
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
-}
+
+	add_filter('wp_nav_menu_objects', function( $items, $args ) {
+	// loop
+		foreach( $items as &$item ) {
+		// vars
+			$icon = get_field('icon', $item);
+		// replace title with icon
+			if( $icon ) {
+				$title = $item->title;
+				$item->title = '<i class="fab fa-'.$icon.'" title="'.$title.'"></i>';	
+			}
+		}
+
+	// return
+		return $items;
+	}, 10, 2);
+	
+endif; /* End ACF related options & filters */
+
+/* Navigation menus */
+add_action( 'after_setup_theme', function() {
+	register_nav_menu( 'social-media', __( 'Social Media Menu', 'heisenberg' ) );
+});
