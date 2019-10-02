c8aea902d66f50db58f1fd6e43894d199ed61bf7
Steve <steve@australiansteve.com>
2019-10-02 08:56:00 -0300

    Fix off-canvas JS so that it doesn't try to change all FA icons on the page

--- assets/js/off-canvas.js
+++ assets/js/off-canvas.js
@@ -1,12 +1,12 @@
 
 jQuery(document).on('opened.zf.offcanvas', function($) {
-	var off_canvas_icon = jQuery(this).find('i');
+	var off_canvas_icon = jQuery(this).find('i.fa-bars');
 
     off_canvas_icon.removeClass('fa-bars').addClass('fa-times');
 });
 
 jQuery(document).on('closed.zf.offcanvas', function() {
-    var off_canvas_icon = jQuery(this).find('i');
+    var off_canvas_icon = jQuery(this).find('i.fa-times');
 
     off_canvas_icon.removeClass('fa-times').addClass('fa-bars');
 });
