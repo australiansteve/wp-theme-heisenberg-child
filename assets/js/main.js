a29f85ae2288210829f544ce7f15201836258e29
Steve <steve@australiansteve.com>
2019-10-14 13:24:25 -0300

    Require new menu walker class, and initialize Foundation by default duh

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,6 @@ import './off-canvas.js'
 
 jQuery( document ).ready(function() {
 
+	jQuery(document).foundation();
+	
 });
--- functions.php
+++ functions.php
@@ -6,3 +6,4 @@
 namespace Heisenberg;
 
 require_once __DIR__ . '/src/enqueue.php';
+require_once __DIR__ . '/src/menu-walker.php';
