a82c6e15a42b3f02f8cc77b6c4c2acaa933bcad7
Steve <steve@australiansteve.com>
2020-04-08 09:23:36 -0300

    Show hamburger menu on every section on small, but not the logo (only on first section)

--- page-templates/template-parts/header.php
+++ page-templates/template-parts/header.php
@@ -1,5 +1,5 @@
 
-<div class="left">
+<div class="left <?php echo ($sectionNumber != 1) ? 'show-for-medium' : '';?>">
 	<a href="<?php echo is_front_page() ? "#" : "/";?>" class="<?php echo is_front_page() ? "home-reset-scroll" : "";?>" title="<?php echo get_bloginfo('name');?>"><img class="header-logo" src="<?php echo $headerImage[0]; ?>" alt=""></a>
 </div>
 <div class="right">
--- page-templates/template-parts/section-header.php
+++ page-templates/template-parts/section-header.php
@@ -6,9 +6,8 @@
 		?>
 <section id="<?php echo $sectionId;?>"  data-section="<?php global $sectionNumber; echo $sectionNumber;?>" class="<?php echo ($sectionNumber == 1) ? 'active' : '';?>">
 
-	<div class="section-header <?php echo ($sectionNumber != 1) ? 'show-for-medium' : '';?>" style="color: <?php echo $customTextColor['text_color'];?>">
+	<div class="section-header" style="color: <?php echo $customTextColor['text_color'];?>">
 		<?php 
-		$sectionNumber++;
 		$defaultLogoId = get_field('default_logo', 'option');
 		if ($customLogo) {
 			$headerImage = wp_get_attachment_image_src( $customLogo , 'header-image' );
@@ -17,5 +16,6 @@
 			$headerImage = wp_get_attachment_image_src( $defaultLogoId , 'header-image' );
 		}
 		include( locate_template( 'page-templates/template-parts/header.php', false, false ) ); 
+		$sectionNumber++;
 		?>
 	</div>
