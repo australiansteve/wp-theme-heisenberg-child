ad9202edfaea2adf34a5c19af506a513f83b76d5
Steve <steve@australiansteve.com>
2020-04-08 15:42:43 -0300

    Make product archive images (any image block) clickable by adding link field in to img component

--- page-templates/template-parts/section-img-with-background.php
+++ page-templates/template-parts/section-img-with-background.php
@@ -5,15 +5,28 @@ $position =  get_field($sectionId.'_position');
 error_log("Image position: ".print_r($position, true));
 $positionV = is_array($position) ? $position['vertical'] : 'center';
 $positionH = is_array($position) ? $position['horizontal'] : 'center';
+$link = get_field($sectionId.'_image_link');
 ?>
 <div class="grid-y align-<?php echo $positionV;?> img-block <?php echo $sectionClasses;?>" id="<?php echo $sectionId;?>" style="height: 100%">
 	<?php
 	include( locate_template( 'page-templates/template-parts/section-background.php', false, false ) ); 
 	?>
-	<div class="cell text-<?php echo $positionH;?>">
+	<div class="cell small-12 text-<?php echo $positionH;?>">
+
+		<?php		 
+		 if( $link ) {
+		 	echo "<a href='".$link."'>";
+		 }
+		 ?>
 		<?php		 
 		 if( $image ) {
 		 	echo wp_get_attachment_image( $image, $size );
+		 }
+		 ?>
+
+		<?php		 
+		 if( $image ) {
+		 	echo "</a>";
 		 }
 		 ?>
 	</div>
