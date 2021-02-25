9534eb8fa9acadf54e74fd9bc68182bca73a60db
Steve <steve@australiansteve.com>
2021-02-25 14:23:07 -0400

    Reposition main logo above main menu

--- header.php
+++ header.php
@@ -73,41 +73,74 @@
 
 		<header id="header" class="grid-container">
 			<div class="content-container" >
-				<div class="grid-x" id="header-language-switch">
-					<div class="cell small-12 medium-5 medium-offset-6 medium-text-right">
-						<?php echo get_template_part('template-parts/search-bar'); ?>
+
+				<div class="grid-x">
+					<div class="cell medium-8 medium-order-2">
+						<div class="grid-x" id="header-language-switch">
+							<div class="cell small-12 medium-5 medium-offset-5 medium-text-right">
+								<?php echo get_template_part('template-parts/search-bar'); ?>
+							</div>
+							<div class="cell small-11 medium-2 small-text-left medium-text-right">
+								<span class="language-fr"><a href="<?php echo $frLink; ?>">FR</a></span> | 
+								<span class="language-en"><a href="<?php echo $enLink; ?>">EN</a></span>
+							</div>
+							<div class="cell small-1 off-canvas-content hide-for-medium small-text-right" id="hamburger-menu" data-off-canvas-content>
+								<a href="#" data-toggle="offCanvasLeft"><i class="fas fa-bars"></i></a>
+							</div>			
+						</div>
+
+						<div class="grid-x show-for-medium" id="header-contact-information">
+							<div class="cell text-right">
+								<span class="telephone-number"><?php the_field('telephone_number_text', 'option')?>: <?php if (get_field('telephone_number_link', 'option')) :?> <a href="<?php the_field('telephone_number_link', 'option')?>"><?php endif; ?><?php the_field('telephone_number', 'option')?><?php if (get_field('telephone_number_link', 'option')) :?></a><?php endif; ?></span>
+								<span class="email-address"><?php the_field('email_address_text', 'option')?>: <a href="mailto:<?php the_field('email_address', 'option')?>"><?php the_field('email_address', 'option')?></a></span>
+							</div>
+							<div class="cell text-right">
+								<?php
+								wp_nav_menu( [
+									'theme_location' => 'social-media-menu',
+									'menu_class' => 'menu horizontal align-right',
+									'menu_id' => 'social-media-menu' ,
+									'items_wrap' => '' ,
+									'items_wrap' => '<div id="%1$s" class="%2$s">%3$s</div>'
+								] ); 
+								?>
+							</div>
+						</div>
+						<div class="grid-x show-for-medium" id="header-links">
+							<div class="cell text-right">
+								<span class="mailing-list"><a href="#" data-open="mailchimpSignupModal"><?php the_field('mailing_list_text', 'option')?></a></span> | 
+								<span class="account-login"><a href="<?php the_field('account_login_link', 'option')?>"><?php the_field('account_login_text', 'option')?></a></span>
+							</div>
+						</div>
 					</div>
-					<div class="cell small-11 medium-1 small-text-left medium-text-right">
-						<span class="language-fr"><a href="<?php echo $frLink; ?>">FR</a></span> | 
-						<span class="language-en"><a href="<?php echo $enLink; ?>">EN</a></span>
+
+					<div class="cell medium-4 medium-order-1">
+						<div class="grid-x" id="header-logo">
+							<div class="cell text-center medium-text-left">
+								<?php
+								if ( function_exists( 'the_custom_logo' ) ) {
+									$custom_logo_id = get_theme_mod( 'custom_logo' );
+									$image = wp_get_attachment_image_src( $custom_logo_id , 'site_logo' );
+									printf( '<a href="%s" rel="home">',	esc_url( home_url( '/' ) ));
+									?>
+									<img class="custom-logo" src="<?php echo $image[0];?>" height="173" width="240" alt="<?php bloginfo( 'name' ); ?>"/>
+									<?php
+									printf( '</a>');
+								}
+								else
+								{
+									printf( '<h1><a href="%s" rel="home">%s</a></h1>',
+										esc_url( home_url( '/' ) ),
+										esc_html( get_bloginfo( 'name' ) )
+									);
+								}
+								?>
+							</div>
+						</div>
 					</div>
-					<div class="cell small-1 off-canvas-content hide-for-medium small-text-right" id="hamburger-menu" data-off-canvas-content>
-						<a href="#" data-toggle="offCanvasLeft"><i class="fas fa-bars"></i></a>
-					</div>			
+					
 				</div>
-				<div class="grid-x show-for-medium" id="header-contact-information">
-					<div class="cell text-right">
-						<span class="telephone-number"><?php the_field('telephone_number_text', 'option')?>: <?php if (get_field('telephone_number_link', 'option')) :?> <a href="<?php the_field('telephone_number_link', 'option')?>"><?php endif; ?><?php the_field('telephone_number', 'option')?><?php if (get_field('telephone_number_link', 'option')) :?></a><?php endif; ?></span>
-						<span class="email-address"><?php the_field('email_address_text', 'option')?>: <a href="mailto:<?php the_field('email_address', 'option')?>"><?php the_field('email_address', 'option')?></a></span>
-					</div>
-					<div class="cell text-right">
-						<?php
-						wp_nav_menu( [
-							'theme_location' => 'social-media-menu',
-							'menu_class' => 'menu horizontal align-right',
-							'menu_id' => 'social-media-menu' ,
-							'items_wrap' => '' ,
-							'items_wrap' => '<div id="%1$s" class="%2$s">%3$s</div>'
-						] ); 
-						?>
-					</div>
-				</div>
-				<div class="grid-x show-for-medium" id="header-links">
-					<div class="cell text-right">
-						<span class="mailing-list"><a href="#" data-open="mailchimpSignupModal"><?php the_field('mailing_list_text', 'option')?></a></span> | 
-						<span class="account-login"><a href="<?php the_field('account_login_link', 'option')?>"><?php the_field('account_login_text', 'option')?></a></span>
-					</div>
-				</div>
+
 				<div class="grid-x show-for-medium" id="header-menu">
 					<div class="cell text-right">
 						<?php
@@ -120,29 +153,8 @@
 						] ); 
 						?>
 					</div>
-				</div>
-				<div class="grid-x" id="header-logo">
-					<div class="cell text-center medium-text-left">
-						<?php
-						if ( function_exists( 'the_custom_logo' ) ) {
-							$custom_logo_id = get_theme_mod( 'custom_logo' );
-							$image = wp_get_attachment_image_src( $custom_logo_id , 'site_logo' );
-							printf( '<a href="%s" rel="home">',	esc_url( home_url( '/' ) ));
-							?>
-							<img class="custom-logo" src="<?php echo $image[0];?>" height="173" width="240" alt="<?php bloginfo( 'name' ); ?>"/>
-							<?php
-							printf( '</a>');
-						}
-						else
-						{
-							printf( '<h1><a href="%s" rel="home">%s</a></h1>',
-								esc_url( home_url( '/' ) ),
-								esc_html( get_bloginfo( 'name' ) )
-							);
-						}
-						?>
-					</div>
-				</div>
+				</div>	
+
 				<div class="grid-x show-for-home-only" id="header-tagline">
 					<div class="cell">
 						<h2><?php the_field('header_tagline_title', 'option'); ?></h2>
