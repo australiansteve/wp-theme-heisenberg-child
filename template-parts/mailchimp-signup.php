cc47a21aa070163b7f3f38ee848a2ecbd5297f69
Steve Weaver Crawford <steve@australiansteve.com>
2024-03-12 22:38:37 -0300

    ACF 6.2.7 compatibility

--- template-parts/mailchimp-signup.php
+++ template-parts/mailchimp-signup.php
@@ -1,8 +1,8 @@
 <div class="reveal" id="mailchimpSignupModal" data-reveal>
 	<h2 id="modalTitle"><?php the_field('mc_dialog_title', 'option');?></h2>
-	<p><?php the_field('mc_dialog_introduction', 'option');?></p>
-	<?php the_field('mc_embed_code', 'option');?>
-	<p><?php the_field('mc_dialog_conclusion', 'option');?></p>
+	<p><?php echo get_field('mc_dialog_introduction', 'option');?></p>
+	<?php echo get_field('mc_embed_code', 'option');?>
+	<p><?php echo get_field('mc_dialog_conclusion', 'option');?></p>
 
 	<button class="close-button" data-close aria-label="Close modal" type="button">
 		<span aria-hidden="true">&times;</span>
